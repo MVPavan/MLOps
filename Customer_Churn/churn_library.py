606dbbb47e82d29d133cf97609179cb8009d6e4f
Pavan MV <venkatapavan.muppala@jci.com>
2022-10-20 13:00:52 +0530

    mlflow in progress

--- Customer_Churn/churn_library.py
+++ Customer_Churn/churn_library.py
@@ -1,15 +1,11 @@
-# library doc string
-
-
-# import libraries
-import shap
-import joblib
-import pytest
-import pandas as pd
+"""
+Churn Library module
+"""
+import os
+from dataclasses import dataclass
+from pathlib import Path
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-
+import pandas as pd
 from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
 
@@ -18,75 +14,113 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import plot_roc_curve, classification_report
-import os
-from pathlib import Path
-import constansts as consts
-os.environ['QT_QPA_PLATFORM']='offscreen'
 
-images_path = Path(__file__).parent/"images"
+import matplotlib.pyplot as plt
+import seaborn as sns
+import shap
+import joblib
+import constansts as consts
+
+sns.set()
+
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+
+images_path = Path(__file__).parent / "images"
 images_path.mkdir(parents=True, exist_ok=True)
 
-models_path = Path(__file__).parent/"models"
+models_path = Path(__file__).parent / "models"
 models_path.mkdir(parents=True, exist_ok=True)
 
 
+@dataclass
+class CLVariables:
+    """
+    Collection of Churn Library Variables
+    """
+
+    def __init__(self):
+        self.X = None
+        self.y = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.lfc = None
+        self.rfc = None
+        self.y_train_preds_rf = None
+        self.y_test_preds_rf = None
+        self.y_train_preds_lr = None
+        self.y_test_preds_lr = None
+
+
 class ChurnLibrary:
-    def __init__(self, pth) -> None:
+    """
+    ChurnLibrary Class
+    """
+
+    def __init__(self, pth):
         '''
-        returns dataframe for the csv found at pth
+        creates dataframe for the csv found at pth
         input:
                 pth: a path to the csv
         output:
-                df: pandas dataframe
-        '''	
-        self.df=pd.read_csv(pth)
+                self.df pandas dataframe
+        '''
+        self.df = pd.read_csv(pth)
+        self.cl_vars = CLVariables()
 
     @staticmethod
     def plot_func(df_column, plot_type, save_name):
         """
         df_column: series to plot
         plot_type: ["hist","bar","sns_hist_density"]
-        return: matplotlib figure
+        saves matplotlib figure
         """
-        fig=plt.figure(figsize=(20,10)) 
-        if plot_type=="hist":
+        fig = plt.figure(figsize=(20, 10))
+        if plot_type == "hist":
             fig = df_column.hist()
-        if plot_type=="bar":
+        if plot_type == "bar":
             fig = df_column.plot(kind="bar")
-        if plot_type=="sns_hist_density":
+        if plot_type == "sns_hist_density":
             # fig = sns.histplot(df_column, stat='density', kde=True)
             fig = sns.distplot(df_column, kde=True)
-        if plot_type=="sns_heatmap":
-            fig = sns.heatmap(df_column.corr(numeric_only=True), annot=False, cmap='Dark2_r', linewidths = 2)
+        if plot_type == "sns_heatmap":
+            fig = sns.heatmap(
+                df_column.corr(
+                    numeric_only=True),
+                annot=False,
+                cmap='Dark2_r',
+                linewidths=2)
 
-        fig.get_figure().savefig(images_path/save_name)
-        return
-        
+        fig.get_figure().savefig(images_path / save_name)
 
-    def perform_eda(self,plots=True):
+    def perform_eda(self, plots=True):
         '''
         perform eda on df and save figures to images folder
         plots: Plots and saves eda graphs
         output: None
         '''
-        self.df['Churn'] = self.df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
-        if not plots:return
+        self.df['Churn'] = self.df['Attrition_Flag'].apply(
+            lambda val: 0 if val == "Existing Customer" else 1)
+        if not plots:
+            return
         ChurnLibrary.plot_func(
-            df_column=self.df["Churn"], 
+            df_column=self.df["Churn"],
             plot_type="hist", save_name=consts.eda_plots["Churn"])
         ChurnLibrary.plot_func(
-            df_column=self.df["Customer_Age"], 
+            df_column=self.df["Customer_Age"],
             plot_type="hist", save_name=consts.eda_plots["Customer_Age"])
         ChurnLibrary.plot_func(
-            df_column=self.df["Marital_Status"].value_counts("normalize"), 
+            df_column=self.df["Marital_Status"].value_counts("normalize"),
             plot_type="bar", save_name=consts.eda_plots["Marital_Status"])
         ChurnLibrary.plot_func(
-            df_column=self.df['Total_Trans_Ct'], 
-            plot_type="sns_hist_density", save_name=consts.eda_plots["Total_Trans_Ct"])
+            df_column=self.df['Total_Trans_Ct'],
+            plot_type="sns_hist_density",
+            save_name=consts.eda_plots["Total_Trans_Ct"])
         ChurnLibrary.plot_func(
-            df_column=self.df, 
+            df_column=self.df,
             plot_type="sns_heatmap", save_name=consts.eda_plots["heatmap"])
-        
+
     def encoder_helper_loop(self):
         '''
         helper function to turn each categorical column into a new column with
@@ -95,38 +129,45 @@ class ChurnLibrary:
         converts catergorical column to numerical/onehot encoding and appends to dataframe
         '''
         for cat_col, cat_col_new in consts.cat_columns.items():
-            self.df[cat_col_new] = self.df[cat_col].map(self.df.groupby(cat_col).mean(numeric_only=True)['Churn'])
+            self.df[cat_col_new] = self.df[cat_col].map(
+                self.df.groupby(cat_col).mean(numeric_only=True)['Churn'])
             consts.quant_columns.append(cat_col_new)
-        
 
     def perform_feature_engineering(self):
         '''
         input:
                 df: pandas dataframe
-                response: string of response name [optional argument that could be used for naming variables or index self.y column]
+                response: string of response name [optional argument that could be
+                used for naming variables or index self.cl_vars.y column]
 
         output:
-                X_train: self.X training data
-                X_test: self.X testing data
-                y_train: self.y training data
-                y_test: self.y testing data
+                X_train: self.cl_vars.X training data
+                X_test: self.cl_vars.X testing data
+                y_train: self.cl_vars.y training data
+                y_test: self.cl_vars.y testing data
         '''
-        self.X = self.df[consts.quant_columns] 
-        self.y = self.df["Churn"]
-        self.X_train, self.X_test, self.y_train, self.y_test =\
-             train_test_split(self.X, self.y, test_size= 0.3, random_state=42)
+        self.cl_vars.X = self.df[consts.quant_columns]
+        self.cl_vars.y = self.df["Churn"]
+        self.cl_vars.X_train, self.cl_vars.X_test, self.cl_vars.y_train, self.cl_vars.y_test =\
+            train_test_split(self.cl_vars.X, self.cl_vars.y, test_size=0.3, random_state=42)
 
     def save_models(self):
-        joblib.dump(self.rfc, models_path/consts.model_names["rfc"])
-        joblib.dump(self.lrc, models_path/consts.model_names["lr"])
-    
+        """
+        Saves Models
+        """
+        joblib.dump(self.cl_vars.rfc, models_path / consts.model_names["rfc"])
+        joblib.dump(self.cl_vars.lrc, models_path / consts.model_names["lr"])
+
     def load_models(self, load_pretrained=True):
+        """
+        Load Models
+        """
         if load_pretrained:
             name_dict = consts.model_names_pretrained
         else:
             name_dict = consts.model_names
-        self.rfc = joblib.load(models_path/name_dict["rfc"])
-        self.lrc = joblib.load(models_path/name_dict["lrc"])
+        self.cl_vars.rfc = joblib.load(models_path / name_dict["rfc"])
+        self.cl_vars.lrc = joblib.load(models_path / name_dict["lrc"])
 
     def classification_report_image(self):
         '''
@@ -137,122 +178,149 @@ class ChurnLibrary:
         '''
         plt.clf()
         plt.rc('figure', figsize=(5, 5))
-        #plt.text(0.01, 0.05, str(model.summary()), {'fontsize': 12}) old approach
-        plt.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 10}, fontproperties = 'monospace')
-        plt.text(0.01, 0.05, str(classification_report(self.y_train, self.y_train_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
-        plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10}, fontproperties = 'monospace')
-        plt.text(0.01, 0.7, str(classification_report(self.y_test, self.y_test_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+        # plt.text(0.01, 0.05, str(model.summary()), {'fontsize': 12}) old
+        # approach
+        plt.text(0.01, 1.25, str('Random Forest Train'), {
+                 'fontsize': 10}, fontproperties='monospace')
+        plt.text(
+            0.01, 0.05, str(
+                classification_report(
+                    self.cl_vars.y_train, self.cl_vars.y_train_preds_rf)), {
+                'fontsize': 10}, fontproperties='monospace')
+        plt.text(0.01, 0.6, str('Random Forest Test'), {
+                 'fontsize': 10}, fontproperties='monospace')
+        plt.text(
+            0.01, 0.7, str(
+                classification_report(
+                    self.cl_vars.y_test, self.cl_vars.y_test_preds_rf)), {
+                'fontsize': 10}, fontproperties='monospace')
         plt.axis('off')
-        plt.savefig(images_path/consts.result_plots["rfc_accuracy"])
+        plt.savefig(images_path / consts.result_plots["rfc_accuracy"])
 
         plt.clf()
         plt.rc('figure', figsize=(5, 5))
-        plt.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 10}, fontproperties = 'monospace')
-        plt.text(0.01, 0.05, str(classification_report(self.y_train, self.y_train_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
-        plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10}, fontproperties = 'monospace')
-        plt.text(0.01, 0.7, str(classification_report(self.y_test, self.y_test_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+        plt.text(0.01, 1.25, str('Logistic Regression Train'),
+                 {'fontsize': 10}, fontproperties='monospace')
+        plt.text(
+            0.01, 0.05, str(
+                classification_report(
+                    self.cl_vars.y_train, self.cl_vars.y_train_preds_lr)), {
+                'fontsize': 10}, fontproperties='monospace')
+        plt.text(0.01, 0.6, str('Logistic Regression Test'), {
+                 'fontsize': 10}, fontproperties='monospace')
+        plt.text(
+            0.01, 0.7, str(
+                classification_report(
+                    self.cl_vars.y_test, self.cl_vars.y_test_preds_lr)), {
+                'fontsize': 10}, fontproperties='monospace')
         plt.axis('off')
-        plt.savefig(images_path/consts.result_plots["lr_accuracy"])
-
+        plt.savefig(images_path / consts.result_plots["lr_accuracy"])
 
     def feature_importance_plot(self):
         '''
         creates and stores the feature importances in pth
-        input:
-                model: model object containing feature_importances_
-                X_data: pandas dataframe of self.X values
-                output_pth: path to store the figure
-
-        output:
-                None
         '''
         plt.clf()
-        explainer = shap.TreeExplainer(self.rfc)
-        shap_values = explainer.shap_values(self.X_test)
-        shap.summary_plot(shap_values, self.X_test, plot_type="bar", show=False)
-        plt.savefig(images_path/consts.result_plots["shap"])
+        explainer = shap.TreeExplainer(self.cl_vars.rfc)
+        shap_values = explainer.shap_values(self.cl_vars.X_test)
+        shap.summary_plot(
+            shap_values,
+            self.cl_vars.X_test,
+            plot_type="bar",
+            show=False)
+        plt.savefig(images_path / consts.result_plots["shap"])
 
         # Calculate feature importances
-        importances = self.rfc.feature_importances_
+        importances = self.cl_vars.rfc.feature_importances_
         # Sort feature importances in descending order
         indices = np.argsort(importances)[::-1]
         # Rearrange feature names so they match the sorted feature importances
-        names = [self.X.columns[i] for i in indices]
+        names = [self.cl_vars.X.columns[i] for i in indices]
         # Create plot
-        fig=plt.figure(figsize=(20,5))
+        fig = plt.figure(figsize=(20, 5))
         # Create plot title
         plt.title("Feature Importance")
         plt.ylabel('Importance')
 
         # Add bars
-        plt.bar(range(self.X.shape[1]), importances[indices])
+        plt.bar(range(self.cl_vars.X.shape[1]), importances[indices])
         # Add feature names as x-axis labels
-        plt.xticks(range(self.X.shape[1]), names, rotation=90)
-        fig.get_figure().savefig(images_path/consts.result_plots["features"])
+        plt.xticks(range(self.cl_vars.X.shape[1]), names, rotation=90)
+        fig.get_figure().savefig(images_path / consts.result_plots["features"])
 
     def roc_plot(self):
         '''
         creates and stores the feature importances in pth
-        input:
-                output_pth: path to store the figure
-        output:
-                None
         '''
-        lrc_plot = plot_roc_curve(self.lrc, self.X_test, self.y_test)
+        lrc_plot = plot_roc_curve(
+            self.cl_vars.lrc,
+            self.cl_vars.X_test,
+            self.cl_vars.y_test)
 
         fig = plt.figure(figsize=(15, 8))
         ax = plt.gca()
-        rfc_disp = plot_roc_curve(self.rfc, self.X_test, self.y_test, ax=ax, alpha=0.8)
+        rfc_disp = plot_roc_curve(
+            self.cl_vars.rfc,
+            self.cl_vars.X_test,
+            self.cl_vars.y_test,
+            ax=ax,
+            alpha=0.8)
         lrc_plot.plot(ax=ax, alpha=0.8)
-        fig.get_figure().savefig(images_path/consts.result_plots["roc"])
+        fig.get_figure().savefig(images_path / consts.result_plots["roc"])
 
     def train_models(self):
         '''
         train, store model results: images + scores, and store models
-        input:
-                X_train: self.X training data
-                X_test: self.X testing data
-                y_train: self.y training data
-                y_test: self.y testing data
-        output:
-                None
         '''
         # grid search
-        self.rfc = RandomForestClassifier(random_state=42)
+        self.cl_vars.rfc = RandomForestClassifier(random_state=42)
         # Use a different solver if the default 'lbfgs' fails to converge
-        # Reference: https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
-        self.lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
+        # Reference:
+        # https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+        self.cl_vars.lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
 
-        param_grid = { 
+        param_grid = {
             'n_estimators': [200, 500],
             'max_features': ['sqrt'],
-            'max_depth' : [4,5,100],
-            'criterion' :['gini', 'entropy']
+            'max_depth': [4, 5, 100],
+            'criterion': ['gini', 'entropy']
         }
 
-        self.cv_rfc = GridSearchCV(estimator=self.rfc, param_grid=param_grid, cv=5)
-        self.cv_rfc.fit(self.X_train, self.y_train)
-        self.rfc = self.cv_rfc.best_estimator_
-        self.lrc.fit(self.X_train, self.y_train)
+        cv_rfc = GridSearchCV(
+            estimator=self.cl_vars.rfc, param_grid=param_grid, cv=5)
+        cv_rfc.fit(self.cl_vars.X_train, self.cl_vars.y_train)
+        self.cl_vars.rfc = cv_rfc.best_estimator_
+        self.cl_vars.lrc.fit(self.cl_vars.X_train, self.cl_vars.y_train)
 
         self.save_models()
 
-        self.y_train_preds_rf = self.rfc.predict(self.X_train)
-        self.y_test_preds_rf = self.rfc.predict(self.X_test)
-
-        self.y_train_preds_lr = self.lrc.predict(self.X_train)
-        self.y_test_preds_lr = self.lrc.predict(self.X_test)
+        self.cl_vars.y_train_preds_rf = self.cl_vars.rfc.predict(
+            self.cl_vars.X_train)
+        self.cl_vars.y_test_preds_rf = self.cl_vars.rfc.predict(
+            self.cl_vars.X_test)
+        self.cl_vars.y_train_preds_lr = self.cl_vars.lrc.predict(
+            self.cl_vars.X_train)
+        self.cl_vars.y_test_preds_lr = self.cl_vars.lrc.predict(
+            self.cl_vars.X_test)
 
     def infer_models(self):
+        """
+        Infer Models
+        """
         self.load_models()
-        self.y_train_preds_rf = self.rfc.predict(self.X_train)
-        self.y_test_preds_rf = self.rfc.predict(self.X_test)
+        self.cl_vars.y_train_preds_rf = self.cl_vars.rfc.predict(
+            self.cl_vars.X_train)
+        self.cl_vars.y_test_preds_rf = self.cl_vars.rfc.predict(
+            self.cl_vars.X_test)
 
-        self.y_train_preds_lr = self.lrc.predict(self.X_train)
-        self.y_test_preds_lr = self.lrc.predict(self.X_test)
+        self.cl_vars.y_train_preds_lr = self.cl_vars.lrc.predict(
+            self.cl_vars.X_train)
+        self.cl_vars.y_test_preds_lr = self.cl_vars.lrc.predict(
+            self.cl_vars.X_test)
 
 
 if __name__ == "__main__":
+    pass
     churn_lib = ChurnLibrary("/data/MLOps/Customer_Churn/data/bank_data.csv")
     churn_lib.perform_eda()
     churn_lib.encoder_helper_loop()
--- Customer_Churn/churn_script_logging_and_tests.py
+++ Customer_Churn/churn_script_logging_and_tests.py
@@ -6,31 +6,34 @@ import churn_library as churnlib
 
 test_log = logging.getLogger(__name__)
 
-backup_path = Path(__file__).parent/"backup"
+backup_path = Path(__file__).parent / "backup"
 backup_path.mkdir(parents=True, exist_ok=True)
+
 
 class Vars:
     bank_data_file = "./data/bank_data.csv"
 
 
-def log_assert(expression,msg):
+def log_assert(expression, msg):
     """
     Function which asserts given expression and
     logs provided message if assertion fails
     """
-    if expression:return
+    if expression:
+        return
     test_log.error(msg)
     assert expression, msg
+
 
 def log_try_except(func, error, error_msg):
     """
     executes func in try and except block
     if rasied error matches provide error logs the error msg
-    usage: log_try_except(function, ErrorType, "error message")(function arguments) 
+    usage: log_try_except(function, ErrorType, "error message")(function arguments)
     """
     def wrapper(*args, **kwargs):
         try:
-            return func(*args,**kwargs)
+            return func(*args, **kwargs)
         except error as err:
             test_log.error(error_msg)
             raise err
@@ -47,41 +50,47 @@ def churn_library_object():
 
 def test_import():
     """
-	test data import - this example is completed for you to assist with the other test functions
-	"""
+        test data import - this example is completed for you to assist with the other test functions
+        """
     df = log_try_except(
-        func = churnlib.ChurnLibrary,
+        func=churnlib.ChurnLibrary,
         error=FileNotFoundError,
         error_msg="Testing import_eda: The file wasn't found",
     )(Vars.bank_data_file).df
 
     test_log.info("Testing import_data: SUCCESS")
 
-    log_assert(df.shape[0] > 0, "Testing import_data: The file doesn't appear to have rows and columns")
-    log_assert(df.shape[1] > 0, "Testing import_data: The file doesn't appear to have rows and columns")
+    log_assert(
+        df.shape[0] > 0,
+        "Testing import_data: The file doesn't appear to have rows and columns")
+    log_assert(
+        df.shape[1] > 0,
+        "Testing import_data: The file doesn't appear to have rows and columns")
 
 
 def test_eda(churn_library_object):
     '''
-	test perform eda function
-	'''
+        test perform eda function
+        '''
     # Delete existing image files
     image_names = churnlib.consts.eda_plots.values()
     for fp in churnlib.images_path.iterdir():
-        if fp.name in image_names: fp.rename(backup_path/f"backup_{fp.name}")
+        if fp.name in image_names:
+            fp.rename(backup_path / f"backup_{fp.name}")
 
     churn_library_object.perform_eda()
-    image_path_list = [fp.name for fp in churnlib.images_path.iterdir() if fp.is_file()]
+    image_path_list = [
+        fp.name for fp in churnlib.images_path.iterdir() if fp.is_file()]
     for fname in image_names:
-        log_assert(fname in image_path_list,f"{fname} not generated in EDA")
-    
+        log_assert(fname in image_path_list, f"{fname} not generated in EDA")
+
     test_log.info("Testing EDA: SUCCESS")
-    
+
 
 def test_encoder_helper(churn_library_object):
     '''
-	test encoder helper
-	'''
+        test encoder helper
+        '''
     churn_library_object.encoder_helper_loop()
     for cat_col, cat_col_new in churnlib.consts.cat_columns.items():
         log_assert(cat_col_new in churn_library_object.df.columns, f"No encoding done for {cat_col}")
@@ -91,42 +100,45 @@ def test_encoder_helper(churn_library_object):
 
 def test_perform_feature_engineering(churn_library_object):
     '''
-	test perform_feature_engineering
-	'''
+        test perform_feature_engineering
+        '''
     churn_library_object.perform_feature_engineering()
-    log_assert(
-        set(churn_library_object.X.columns.values) == set(churnlib.consts.quant_columns),
-        "Issue in selection of numerical columns"
-    )
+    log_assert(set(churn_library_object.cl_vars.X.columns.values) == set(
+        churnlib.consts.quant_columns), "Issue in selection of numerical columns")
     test_log.info("Testing Feature Engineering: SUCCESS")
 
 
 def test_train_models(churn_library_object):
     '''
-	test train_models
-	'''
+        test train_models
+        '''
     # Backup existing models
-    for key,val in churnlib.consts.model_names.items():
-        Path(churnlib.models_path/val).rename(backup_path/f"backup_{val}")
+    for key, val in churnlib.consts.model_names.items():
+        Path(churnlib.models_path / val).rename(backup_path / f"backup_{val}")
     churn_library_object.train_models()
-    models_path_list = [fp.name for fp in churnlib.models_path.iterdir() if fp.is_file()]
-    for key,val in churnlib.consts.model_names.items():
-         log_assert(val in models_path_list,f"{val} model not generated during training")
-    
+    models_path_list = [
+        fp.name for fp in churnlib.models_path.iterdir() if fp.is_file()]
+    for key, val in churnlib.consts.model_names.items():
+        log_assert(val in models_path_list, f"{val} model not generated during training")
+
     test_log.info("Testing Model training: SUCCESS")
 
+
 def test_results(churn_library_object):
-    for key,val in churnlib.consts.result_plots.items():
-        Path(churnlib.images_path/val).rename(backup_path/f"backup_{val}")
-    
+    for key, val in churnlib.consts.result_plots.items():
+        Path(churnlib.images_path / val).rename(backup_path / f"backup_{val}")
+
     churn_library_object.roc_plot()
     churn_library_object.feature_importance_plot()
     churn_library_object.classification_report_image()
 
-    results_path_list = [fp.name for fp in churnlib.images_path.iterdir() if fp.is_file()]
-    for key,val in churnlib.consts.result_plots.items():
-         log_assert(val in results_path_list,f"{val} not generated after training")
-   
+    results_path_list = [
+        fp.name for fp in churnlib.images_path.iterdir() if fp.is_file()]
+    for key, val in churnlib.consts.result_plots.items():
+        log_assert(val in results_path_list, f"{val} not generated after training")
+
+    test_log.info("Testing Report Generation: SUCCESS")
+
 
 if __name__ == "__main__":
     pass
